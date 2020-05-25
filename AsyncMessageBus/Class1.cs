9bb9673f51a90d08a492cb9fa09b39ffac447dde
modmynitro <modmynitro@gmail.com>
2020-05-25 23:57:11 +0200

    Add solution and project

---
+++ AsyncMessageBus/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AsyncMessageBus
+{
+    public class Class1
+    {
+    }
+}
---
+++ AsyncMessageBus/Interfaces/IAsyncMessageBus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncMessageBus.Interfaces
+{
+    public interface IAsyncMessageBus
+    {
+    }
+}
